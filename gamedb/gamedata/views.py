bdf588a3cf7c2ca16cc8e8d6f0ddaa43e4298ed9
GianK128 <giankeberlein@gmail.com>
2022-08-30 02:26:40 -0300

    Create views for gamedata

--- gamedb/gamedata/views.py
+++ gamedb/gamedata/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from gamedata.models import Genre, Publisher, Platform, VideoGame
+from gamedata.serializers import GenreSerializer, PublisherSerializer, PlatformSerializer, VideoGameSerializer
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class PublisherViewSet(viewsets.ModelViewSet):
+    queryset = Publisher.objects.all()
+    serializer_class = PublisherSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class PlatformViewSet(viewsets.ModelViewSet):
+    queryset = Platform.objects.all()
+    serializer_class = PlatformSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class VideoGameViewSet(viewsets.ModelViewSet):
+    queryset = VideoGame.objects.all()
+    serializer_class = VideoGameSerializer
+    permission_classes = [permissions.IsAuthenticated]
