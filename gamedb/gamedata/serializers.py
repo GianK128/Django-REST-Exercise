82ff9551e78f7ce665ae4ba25eb5fe11ab18f9f1
GianK128 <giankeberlein@gmail.com>
2022-09-01 00:45:03 -0300

    Upload testing of diff internal value and reps

--- gamedb/gamedata/serializers.py
+++ gamedb/gamedata/serializers.py
@@ -3,28 +3,59 @@ from gamedata.models import Genre, Publisher, Platform, VideoGame
 
 # Modelo de género
 class GenreSerializer(serializers.ModelSerializer):    
+    def __str__(self) -> str:
+        return f"{self.name}"
+    
     class Meta:
         model = Genre
         fields = ['name', 'description', 'created_at', 'updated_at', 'games']
 
 # Modelo de publisher
 class PublisherSerializer(serializers.ModelSerializer):
+    def __str__(self) -> str:
+        return f"{self.trade_name}"
+    
     class Meta:
         model = Publisher
         fields = ['trade_name', 'founded', 'created_at', 'updated_at', 'games']
 
 # Modelo de plataforma
 class PlatformSerializer(serializers.ModelSerializer):
+    def __str__(self) -> str:
+        return f"{self.name}"
+    
     class Meta:
         model = Platform
         fields = ['name', 'manufacturer', 'created_at', 'updated_at', 'games']
 
 # Modelo de videojuego
 class VideoGameSerializer(serializers.ModelSerializer):
-    genres = GenreSerializer(many=True)
+    genres = serializers.SlugRelatedField(
+        many=True,
+        slug_field='name',
+        queryset=Genre.objects.all()
+    )
     publisher = PublisherSerializer()
-    platform = PlatformSerializer(many=True)
+    # publisher_id = serializers.PrimaryKeyRelatedField(
+    #     write_only=True,
+    #     source='publisher',
+    #     queryset=Publisher.objects.all()
+    # )
+    platform = serializers.SlugRelatedField(
+        many=True,
+        slug_field='name',
+        queryset=Platform.objects.all()
+    )
     
+    def to_internal_value(self, data):
+        self.fields['publisher'] = serializers.PrimaryKeyRelatedField(queryset=Publisher.objects.all())
+        return super().to_internal_value(data)
+
+    def to_representation(self, validated_data):
+        response = super().to_representation(validated_data)
+        response['publisher'] = PublisherSerializer(validated_data.publisher).data
+        return response
+
     class Meta:
         model = VideoGame
-        fields = ['name', 'published_year', 'genres', 'publisher', 'platform', 'created_at', 'updated_at']
+        fields = ['name', 'published_year', 'genres', 'publisher', 'publisher_id', 'platform', 'created_at', 'updated_at']
